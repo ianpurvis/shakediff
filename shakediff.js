c91b26e5cf01ec50fd8e6b2371ece066012c4114
Ian Purvis <ian@purvisresearch.com>
2020-09-02 16:01:45 -0500

    Replaces jsdiff with spawned diff

--- shakediff.js
+++ shakediff.js
@@ -1,17 +1,12 @@
 #!/usr/bin/env node
 
-import colors from 'colors/safe.js'
-import { isatty } from 'tty'
-import { readFile } from 'fs/promises'
-import { resolve } from 'path'
+import { spawn } from 'child_process'
+import { createHash } from 'crypto';
+import { readFile, unlink, writeFile } from 'fs/promises'
+import { tmpdir } from 'os'
+import { basename, join } from 'path'
 import { rollup } from 'rollup'
 import virtual from '@rollup/plugin-virtual'
-
-// TODO Replace require with import once jsdiff releases fix
-// https://github.com/kpdecker/jsdiff/issues/292
-import { createRequire } from 'module'
-const require = createRequire(import.meta.url)
-const { structuredPatch } = require('diff')
 
 
 async function main(argv) {
@@ -37,30 +32,23 @@ async function main(argv) {
     preserveModules: true
   })
   const chunk = output.find(chunk => chunk.name == '_virtual:moduleCode')
-  const stats = chunk.modules[chunk.facadeModuleId]
-  const patch = structuredPatch(modulePath, modulePath, moduleCode, chunk.code)
+  const chunkBuffer = Buffer.from(chunk.code, 'utf8')
+  const chunkHash = sha1(chunkBuffer).slice(0, 6)
+  const tempPath = join(tmpdir(), `${chunkHash}_${basename(modulePath)}`)
+  await writeFile(tempPath, chunkBuffer)
+  const exitCode = await spiff(modulePath, tempPath)
+  await unlink(tempPath)
 
-  printPatch(patch, console.log)
+  return exitCode
 }
 
 
-function printPatch(patch, out = console.log) {
-  if (!isatty(1))
-    colors.disable()
-
-  let color = colors.bold
-  out(color(`--- ${patch.oldFileName}`))
-  out(color(`+++ ${patch.newFileName} (shaken)`))
-  for (const {oldStart, oldLines, newStart, newLines, lines} of patch.hunks) {
-    color = colors.cyan
-    out(color(`@@ -${oldStart},${oldLines} +${newStart},${newLines} @@`))
-    for (const line of lines) {
-      color = line.startsWith('-') ? colors.red
-        : line.startsWith('+') ? colors.green
-        : colors.reset
-      out(color(line))
-    }
-  }
+async function spiff(pathA, pathB) {
+  return new Promise((resolve, reject) => {
+    spawn('diff', [ pathA, pathB ], { stdio: 'inherit' })
+      .once('error', reject)
+      .once('close', resolve)
+  })
 }
 
 
@@ -70,4 +58,16 @@ function scaffoldTest(exports) {
 }
 
 
-main(process.argv).catch(console.error)
+function sha1(buffer) {
+  // Simulates git-hash-object (https://stackoverflow.com/q/552659)
+  const hash = createHash('sha1')
+  hash.update('blob ')
+  hash.update(buffer.length.toString())
+  hash.update('\0')
+  hash.update(buffer)
+  return hash.digest('hex')
+}
+
+
+main(process.argv).then(process.exit)
+  .catch(console.error).then(() => process.exit(1))
